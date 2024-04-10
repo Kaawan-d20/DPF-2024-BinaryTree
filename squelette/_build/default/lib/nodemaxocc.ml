ccb281d0d2932ced6f142a7c93a0d0fd66dfad87
Dany <dany.dudiot@gmail.com>
2024-04-10 16:22:32 +0200

    LETS GO maxocc
    on a la base donc maintenant ca roule tout seul

--- squelette/_build/default/lib/nodemaxocc.ml
+++ squelette/_build/default/lib/nodemaxocc.ml
@@ -2,14 +2,20 @@ type data = int
 type answer = int * int
 type node = { answer : answer; left : int; right : int }
 
+let max_tup : answer -> answer -> answer =
+  fun (a, b) (c,d) ->
+    if a = c then
+      (a, b+d)
+    else if a > c then
+      (a,b)
+    else
+      (c,d)
+
 let create : data -> answer = 
- fun _ -> 
-  (0,0)
+ fun valeur -> (valeur,1)
 
 let combine : node -> node -> node =
- fun _ _ ->
-  { answer = (0,0); left = 0; right = 0 }
+ fun n m -> { answer = max_tup n.answer m.answer; left = n.left; right = m.right }
 
 let to_string : answer -> string = 
- fun _ ->
-  ""
+ fun (a,b) -> "(" ^ string_of_int a ^ ", " ^ string_of_int b ^ ")"
--- squelette/_build/default/test/test_devoir_de_programmation.ml
+++ squelette/_build/default/test/test_devoir_de_programmation.ml
@@ -28,6 +28,19 @@ let test_sum_update3 _ =
 let test_sum_update4 _ =
   assert_equal "20" (Sum.to_string (Sum.query updated 0 7))
 
+let query_max = MaxOcc.create [ 1; 1; -2; 8; -7; 6; -5; 8 ]
+
+let test_max_query1 _ =
+  assert_equal "(8, 1)" (MaxOcc.to_string (MaxOcc.query query_max 0 3))
+
+let test_max_query2 _ =
+  assert_equal "(8, 1)" (MaxOcc.to_string (MaxOcc.query query_max 2 6))
+
+let test_max_query3 _ =
+  assert_equal "(6, 1)" (MaxOcc.to_string (MaxOcc.query query_max 4 6))
+
+let test_max_query4 _ =
+  assert_equal "(8, 2)" (MaxOcc.to_string (MaxOcc.query query_max 0 7))
 
 
 let suite =
@@ -41,6 +54,10 @@ let suite =
          "test_sum_update2" >:: test_sum_update2;
          "test_sum_update3" >:: test_sum_update3;
          "test_sum_update4" >:: test_sum_update4;
+         "test_max_query1" >:: test_max_query1;
+         "test_max_query2" >:: test_max_query2;
+         "test_max_query3" >:: test_max_query3;
+         "test_max_query4" >:: test_max_query4;
        ]
 
 let () = run_test_tt_main suite
