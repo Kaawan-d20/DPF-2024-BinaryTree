c17d54b555aa2cfc6e6b7d314a8a1ee19df15430
Dany <dany.dudiot@gmail.com>
2024-04-10 16:02:58 +0200

    CA FONCTIONNE ENFINNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNNN

--- squelette/lib/query.ml
+++ squelette/lib/query.ml
@@ -10,7 +10,7 @@ module Make (N : NODE) : QUERY_STRUCTURE = struct
   type tree = N.node Sig.binary_tree
   (* Le type Arbre, qui sera défini comme un arbre binaire *)
   
-  
+
 
   let node_of_tree : tree -> N.node =
     fun arbre ->
@@ -22,14 +22,18 @@ module Make (N : NODE) : QUERY_STRUCTURE = struct
     fun noeud filsgauche filsdroit ->
       Node {node=noeud; left_child = filsgauche; right_child = filsdroit};;
 
-  let treeLeaf_of_node : N.node -> tree =
-    fun noeud ->
-      Leaf {node = noeud}
 
+  let getLeft : tree -> tree =
+    fun arbre ->
+      match arbre with
+    | Node n -> n.left_child
+    | _  -> failwith "erreur ";;
 
-
-
-
+    let getRight : tree -> tree =
+      fun arbre ->
+        match arbre with
+      | Node n -> n.right_child
+      | _  -> failwith "erreur ";;
 
     (* Création de l’arbre *)
     (*Résumée :
@@ -38,27 +42,34 @@ module Make (N : NODE) : QUERY_STRUCTURE = struct
        exemple : [3,3,3,3] -> [3,3,2] -> [2,2] -> [1]
        *)
 let create : data list -> tree = 
-  fun liste -> 
+  fun liste ->
+    
+    
    let rec aux =
      fun liste acc nb->
        match liste with
        | [] -> acc
-       | head :: tail -> aux tail (Leaf { node = {N.answer = (N.create head) ; N.left = nb ; N.right = nb } }::acc) (nb+1)
+       | head :: tail -> aux tail (acc@[Leaf { node = {N.answer = (N.create head) ; N.left = nb ; N.right = nb } }]) (nb+1)
        (*SI QUELQUE CHOSE CASSE, REGARDER CA      ^                            ^             ^*)
      in let result = aux liste [] 0 in
+
+
        let rec aux2 : tree list -> tree= 
          fun liste ->
            match liste with
-           | [element] -> element
-           | Node head :: Node mid :: tail -> aux2 ([(Node {node = (N.combine head.node mid.node); left_child = Node head; right_child = Node mid})] @ tail)
-           | Leaf head :: Leaf mid :: tail -> aux2 ([(Node {node = (N.combine head.node mid.node); left_child = Leaf head; right_child = Leaf mid})] @ tail)
-           | _ -> raise Not_found
+           | element :: [] -> element
+           | Node head :: Node mid :: tail -> aux2 (tail @ [(Node {node = (N.combine head.node mid.node); left_child = Node head; right_child = Node mid})] )
+           | Leaf head :: Leaf mid :: tail -> aux2 (tail @ [(Node {node = (N.combine head.node mid.node); left_child = Leaf head; right_child = Leaf mid})])
+           | _ -> failwith "erreur create"
      in aux2 result 
  
  ;;
 
 
   (* Mise à jour d’un élément de la liste *)
+  (*On descend à la feuille qui à le bonne indice,
+     on la remplace par une nouvelle feuille qui à la nouvelle valeur,
+     et en remontant dans la récursion on recrée les Nodes par lesquells on est passée*)
   let update : tree -> data -> int -> tree =
     fun arbre valeur index  ->
       let rec aux = fun arbre valeur index ->
@@ -74,12 +85,55 @@ let create : data list -> tree =
         | Leaf _ -> Leaf { node = {N.answer = (N.create valeur) ; N.left = index ; N.right = index }}
       in aux arbre valeur index
 
+  ;;
+
+  let intervalle_est_compris : int -> int -> int -> int -> bool =
+    fun left right debut fin ->
+      (debut <= left && left <= fin) || (debut <= right && right <= fin) || (debut > left && fin < right)
+  ;;
 
 
 
+
+
+
+  (*On verifie si où l'on est c'est exactement l'interval *)
   let query : tree -> int -> int -> answer =
-   fun arbre debut fin -> 
-    N.create 0
+    fun arbre debut fin -> 
+     let rec aux =
+       fun arbre debut fin ->
+        match arbre with
+        (*Si on arrive sur une feuille, on ne peut pas checker les enfants, et donc elle est inclue, ou on ne devrait pas arriver là*)
+        | Leaf _ -> let feuilleNode = node_of_tree arbre in
+            if intervalle_est_compris feuilleNode.left feuilleNode.right debut fin then
+              feuilleNode
+            else
+              failwith "tu n'es pas censé(e) arriver là"
+        (*Si on arrive sur un noeud*)
+        | Node _ ->
+          let arbreNode = node_of_tree arbre in
+          (* Si le noeud est exactement l'intervalle, alors on le retourne *)
+          if arbreNode.left=debut && arbreNode.right=fin then
+            arbreNode
+          (*Sinon, cas par cas*)
+          else
+            (*Si l'arbre et l'intervalle s'entre-coupent (c-a-d si il y'a le moindre element en commun entre l'arbre et l'intervalle)*)
+            if intervalle_est_compris arbreNode.left arbreNode.right debut fin then
+              let leftNode = node_of_tree (getLeft arbre) and rightNode = node_of_tree (getRight arbre) in
+              (*Si les deux enfants du noeud s'entrecoupent avec l'intervalle, on combine le résultat des appels récursifs de la fonction pour les deux enfants*)
+              if (intervalle_est_compris leftNode.left leftNode.right debut fin) && (intervalle_est_compris rightNode.left rightNode.right debut fin) then
+                N.combine (aux (getLeft arbre) debut fin) (aux (getRight arbre) debut fin)
+              else
+                (*Si seul le sous arbre gauche s'entrecoupe avec l'intervalle, on renvoie le resultat de l'appel récursif sur le sous arbre gauche*)
+                if intervalle_est_compris leftNode.left leftNode.right debut fin then
+                  aux (getLeft arbre) debut fin
+                (*Si seul le sous arbre droit s'entrecoupe avec l'intervalle, on renvoie le resultat de l'appel récursif sur le sous arbre droit*)
+                else
+                  aux (getRight arbre) debut fin
+            (*Erreur utilisateur, l'intervalle est externe a l'arbre, on ne devrait pas arriver à ce cas en récursion*)
+            else
+              failwith "L'intervale donnée est en dehors de l'arbre"
+     in (aux arbre debut fin).answer
 
   let to_string = N.to_string
   
