9b560af987588232d784333dc953b5d85d6f5377
Dany <dany.dudiot@gmail.com>
2024-04-10 13:52:55 +0200

    NodeSum + query (create + update)

--- squelette/_build/default/lib/query.ml
+++ squelette/_build/default/lib/query.ml
@@ -11,32 +11,7 @@ module Make (N : NODE) : QUERY_STRUCTURE = struct
   (* Le type Arbre, qui sera défini comme un arbre binaire *)
   
   
-
-  let node_of_tree : tree -> N.node =
-    fun arbre ->
-      match arbre with
-      | Node n -> n.node
-      | Leaf n -> n.node
-    ;;
-  let treeNode_of_node : N.node -> tree -> tree -> tree =
-    fun noeud filsgauche filsdroit ->
-      Node {node=noeud; left_child = filsgauche; right_child = filsdroit};;
-
-  let treeLeaf_of_node : N.node -> tree =
-    fun noeud ->
-      Leaf {node = noeud}
-
-
-
-
-
-
     (* Création de l’arbre *)
-    (*Résumée :
-       la première fonction aux permet de transformer la data list en liste de tree
-       la deuxième partie fait la fusion des arbres et remet à la fin le nouvel arbre crée
-       exemple : [3,3,3,3] -> [3,3,2] -> [2,2] -> [1]
-       *)
 let create : data list -> tree = 
   fun liste -> 
    let rec aux =
@@ -57,28 +32,13 @@ let create : data list -> tree =
  
  ;;
 
-
   (* Mise à jour d’un élément de la liste *)
   let update : tree -> data -> int -> tree =
-    fun arbre valeur index  ->
-      let rec aux = fun arbre valeur index ->
-        match arbre with
-        | Node n -> 
-            if (node_of_tree n.left_child).right >= index && (node_of_tree n.left_child).left <= index then
-              let newArbre = aux n.left_child valeur index in
-              treeNode_of_node (N.combine (node_of_tree newArbre) (node_of_tree n.right_child)) newArbre n.right_child
-            else
-              let newArbre = aux n.right_child valeur index in
-              treeNode_of_node (N.combine (node_of_tree n.left_child) (node_of_tree newArbre)) n.left_child newArbre
-  
-        | Leaf _ -> Leaf { node = {N.answer = (N.create valeur) ; N.left = index ; N.right = index }}
-      in aux arbre valeur index
-
-
-
+   fun _ _ _  ->
+    Leaf { node = { answer = N.create 0 ; left = 0 ; right = 0 } }
 
   let query : tree -> int -> int -> answer =
-   fun arbre debut fin -> 
+   fun _ _ _ -> 
     N.create 0
 
   let to_string = N.to_string
